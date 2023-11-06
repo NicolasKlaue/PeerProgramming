8e0310d0da65e943e66dc2a38a0a2d44332ce127
Nicolas Klaue <klaue.nico@gmail.com>
2023-11-06 16:58:47 +0100

    NewEmployee and optimized imports

--- front/src/main/java/com/example/application/views/main/MainView.java
+++ front/src/main/java/com/example/application/views/main/MainView.java
@@ -4,24 +4,14 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-//#region Imports
-import java.time.LocalDate;
 import java.util.ArrayList;
 
 import com.nimbusds.jose.shaded.gson.Gson;
-import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.combobox.ComboBox;
-import com.vaadin.flow.component.datepicker.DatePicker;
-import com.vaadin.flow.component.dialog.Dialog;
-import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
 import com.vaadin.flow.component.tabs.TabSheet;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
